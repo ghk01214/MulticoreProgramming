00a6fe582bdaf4eb65275446614afd24035b7d31
aida <ghk01214@naver.com>
2022-11-08 17:36:21 +0900

    Lish.h -> List.hpp

--- Lecture/List.hpp
+++ Lecture/List.hpp
@@ -1,7 +1,28 @@
 #pragma once
 
 #include "pch.h"
-#include "List.h"
+#include "Node.h"
+
+template<typename T>
+class List
+{
+public:
+	List();
+	~List();
+
+	bool insert(T value);
+	bool remove(T value);
+	bool contains(T value);
+	void clear();
+
+	void Print();
+private:
+	void Find(T value, Node<T>*& prev, Node<T>*& current);
+
+private:
+	Node<T> _head;
+	Node<T> _tail;
+};
 
 template<typename T>
 List<T>::List()
@@ -9,7 +30,7 @@ List<T>::List()
 	_head.data = std::numeric_limits<int>::min();
 	_tail.data = std::numeric_limits<int>::max();
 
-	_head.next = &_tail;
+	_head.next = MarkableReference<T>{ false, &_tail };
 }
 
 template<typename T>
@@ -20,111 +41,125 @@ List<T>::~List()
 template<typename T>
 inline bool List<T>::insert(T value)
 {
-	Node<T>* prev{ &_head };
-
-	ml.lock();
-	Node<T>* current{ prev->next };
-
-	while (current->data < value)
+	while (true)
 	{
-		prev = current;
-		current = current->next;
+		Node<T>* prev;
+		Node<T>* current;
+
+		Find(value, prev, current);
+
+		if (current->data == value)
+			return false;
+
+		Node<T>* node{ new Node<T>{ value, current } };
+
+		if (prev->next.cas(current, node, false, false) == true)
+			return true;
+
+		delete node;
 	}
-
-	if (current->data != value)
-	{
-		Node<T>* node{ new Node<T>{ value } };
-
-		node->next = current;
-		prev->next = node;
-
-		ml.unlock();
-		return true;
-	}
-
-	ml.unlock();
-	return false;
 }
 
 template<typename T>
 inline bool List<T>::remove(T value)
 {
-	Node<T>* prev{ &_head };
-	ml.lock();
-	Node<T>* current{ prev->next };
-
-	while (current->data < value)
+	while (true)
 	{
-		prev = current;
-		current = current->next;
+		Node<T>* prev;
+		Node<T>* current;
+
+		Find(value, prev, current);
+
+		if (current->data != value)
+			return false;
+		
+		Node<T>* success{ current->next.get_ptr() };
+
+		if (current->next.try_change_mark(success, true) == false)
+			continue;
+
+		prev->next.cas(current, success, false, false);
+
+		return true;
 	}
-
-	if (current->data != value)
-	{
-		ml.unlock();
-		return false;
-	}
-
-	prev->next = current->next;
-
-	delete current;
-
-	ml.unlock();
-	return true;
 }
 
 template<typename T>
 inline bool List<T>::contains(T value)
 {
-	Node<T>* prev{ &_head };
-	ml.lock();
-	Node<T>* current{ prev->next };
+	Node<T>* current{ _head.next.get_ptr() };
+	bool removed;
 
 	while (current->data < value)
 	{
-		prev = current;
-		current = current->next;
+		current = current->next.get_ptr();
+		removed = current->next.get_removed();
 	}
-
-	if (current->data != value)
-	{
-		ml.unlock();
-		return false;
-	}
-
-	ml.unlock();
-	return true;
+	
+	return current->data == value and removed == false;
 }
 
 template<typename T>
 inline void List<T>::clear()
 {
-	Node<T>* p{ _head.next };
+	Node<T>* node{ _head.next.get_ptr() };
 
-	while (p != &_tail)
+	while (node != &_tail)
 	{
-		Node<T>* t{ p };
+		Node<T>* temp{ node };
+		node = node->next.get_ptr();
 
-		p = p->next;
-		delete t;
+		delete temp;
 	}
 
-	_head.next = &_tail;
+	_head.next = MarkableReference<T>{ false, &_tail };
 }
 
 template<typename T>
 inline void List<T>::Print()
 {
-	Node<T>* p{ _head.next };
+	Node<T>* node{ _head.next.get_ptr() };
 
 	for (int32_t i = 0; i < 20; ++i)
 	{
-		if (p != &_tail)
+		if (node != &_tail)
 		{
-			std::cout << std::format("{}, ", p->data);
-			p = p->next;
+			std::cout << std::format("{}, ", node->data);
+			node = node->next.get_ptr();
 		}
 	}
 
 	std::cout << std::endl;
+}
+
+template<typename T>
+inline void List<T>::Find(T value, Node<T>*& prev, Node<T>*& current)
+{
+	while (true)
+	{
+	retry:
+		prev = &_head;
+		current = prev->next.get_ptr();
+
+		while (true)
+		{
+			bool removed;
+			Node<T>* success{ current->next.get_ptr_n_mark(&removed) };
+
+			while (removed == true)
+			{
+				if (prev->next.cas(current, success, false, false) == false)
+					goto retry;
+
+				current = success;
+				success = current->next.get_ptr_n_mark(&removed);
+			}
+
+			if (current->data >= value)
+				return;
+
+			prev = current;
+			current = success;
+		}
+	}
 }
