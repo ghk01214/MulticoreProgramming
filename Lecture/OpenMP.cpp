1e4ba62fc95c5636d87197042ec0dd49e77e4148
aida <ghk01214@naver.com>
2022-12-06 18:59:40 +0900

    OpenMP

---
+++ Lecture/OpenMP.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+#include <format>
+
+#include <omp.h>
+
+int main()
+{
+	volatile int32_t sum{};
+
+#pragma omp parallel
+	{
+		for (int32_t i = 0; i < 50000000 / omp_get_num_threads(); ++i)
+		{
+#pragma omp critical
+			sum += 2;
+		}
+	}
+
+	std::cout << sum << std::endl;
+}
